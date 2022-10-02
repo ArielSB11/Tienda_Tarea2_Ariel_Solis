886f5912ae3af268e64b4ef88bf0637f9abb34ce
ArielSB11 <100382945+ArielSB11@users.noreply.github.com>
2022-10-02 12:53:24 -0600

    Tarea 2 Ariel Solis Bermudez semana 3 del curso Martes CIII 2022

---
+++ src/main/java/com/Version1Proyecto/Version1ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.Version1Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Version1ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Version1ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Version1Proyecto/Version1ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Version1Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Version1ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
